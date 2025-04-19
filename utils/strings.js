bf924def099e90f0cb245da5d78b8d63aa63d695
Pedro D <josepedrodiaz@gmail.com>
2025-04-18 22:27:52 -0300

    feat: create HomeScreen with multilanguage support

---
+++ utils/strings.js
@@ -0,0 +1,22 @@
+import * as Localization from 'expo-localization';
+
+const lang = Localization.locale.startsWith('es') ? 'es' : 'en';
+
+const strings = {
+  es: {
+    splashTitle: "Latero",
+    splashSubtitle: "Recordatorios, en el momento justo.",
+    getStarted: "Empezar",
+    myReminders: "Mis Recordatorios",
+    newReminder: "➕ Nuevo Recordatorio",
+  },
+  en: {
+    splashTitle: "Latero",
+    splashSubtitle: "Reminders, right on time.",
+    getStarted: "Get Started",
+    myReminders: "My Reminders",
+    newReminder: "➕ New Reminder",
+  }
+};
+
+export default strings[lang];
